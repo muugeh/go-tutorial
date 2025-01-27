539cbead73760af5b4d4df2586565f0abd089e8b
muugeh <muugeh@gmail.com>
2025-01-27 15:43:33 +0900

    Add return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -10,10 +10,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
